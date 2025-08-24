126a092f06e9dc7456909958b2429e9c20c6f450
Simjrn <simonjenni374@gmail.com>
2025-08-24 16:34:24 +0100

    Update MDL.py

--- MDL.py
+++ MDL.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from time import sleep
 
+st.set_page_config(icon="🧑‍💻", name="MDL editor")
+
 st.title("DML code editor")
 
 stop = st.toggle("Stop running code at errors?")
